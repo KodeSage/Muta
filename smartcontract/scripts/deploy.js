0dcad9e25104faa0eaf2db9da352f7b74212e3fc
James Harrison <somtochukwujames2000@gmail.com>
2023-01-08 22:51:27 +0100

    chores: subgraphs initiated

--- smartcontract/scripts/deploy.js
+++ smartcontract/scripts/deploy.js
@@ -4,7 +4,8 @@ const main = async () => {
 	const MutaFactory = await hre.ethers.getContractFactory("Muta");
 	const MutaContract = await MutaFactory.deploy();
 	await MutaContract.deployed();
-	console.log("Contract deployed to:", MutaContract.address); // 0x4BDc94De936E0A51c7E367bcE1d9EA5cDed6a249 - Address of Muta contract
+  console.log( "Contract deployed to:", MutaContract.address ); // 0x4BDc94De936E0A51c7E367bcE1d9EA5cDed6a249 - Address of Muta contract
+  
 };
 const runMain = async () => {
   try {
